dec05094458572a3a345abae6e1b8c7957ac5170
Kadu <kadu.silva2014@gmail.com>
2023-04-17 17:17:27 -0300

    ✨ feat: add QR Code Reader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+import { QrReader, OnResultFunction } from "react-qr-reader";
+import { useRouter } from 'next/navigation'
+
 export default function Home() {
-  console.log('Home');
+  const router = useRouter()
+  const handleQr: OnResultFunction = (result, _) => {
+    if (!!result) {
+      router.push(`/service/${result.getText()}`)
+    }
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Hello World
+      <QrReader
+        scanDelay={5000}
+        onResult={handleQr}
+        constraints={{ facingMode: "environment" }}
+        className="w-48"
+      />
     </main>
-  )
+  );
 }

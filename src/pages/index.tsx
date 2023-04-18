c1860e9e5d30a9b3139444048e58040bc284e166
Kadu <kadu.silva2014@gmail.com>
2023-04-18 14:44:26 -0300

    🐛 fix: update index.tsx - added error handling in handleQr and code adjustments
    This commit included error handling in the handleQr callback to handle possible errors during QR
    code reading, and code adjustments to improve the readability and appearance of the QR Reader
    component. The new version of the code also updated the scanDelay property to 500ms to reduce the
    waiting time for QR code reading and increase the efficiency of the component.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,27 @@
-import { QrReader, OnResultFunction, } from "react-qr-reader";
-import { useRouter } from 'next/router'
+import { QrReader, OnResultFunction } from "react-qr-reader";
+import { useRouter } from "next/router";
 
 export default function Home() {
-  const router = useRouter()
-  const handleQr: OnResultFunction = (result, _) => {
-    if (!!result) {
-      router.push(`/service/${result.getText()}`)
+  const router = useRouter();
+  const handleQr: OnResultFunction = (result, error) => {
+    if (result) {
+      router.push({
+        pathname: `/service/${result.getText()}`,
+      });
     }
-  }
+
+    if (error) {
+      console.error("🚀 ~ file: index.tsx:26 ~ Home ~ error:", error);
+    }
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <QrReader
-        scanDelay={5000}
+        scanDelay={500}
         onResult={handleQr}
         constraints={{ facingMode: "environment" }}
-        className="w-48"
+        className="w-52"
       />
     </main>
   );

2ea9bfe1293a448783d993ba61d532ebcf6681ce
Kadu <kadu.silva2014@gmail.com>
2023-04-18 17:32:05 -0300

    ✨ feat: update types.ts with new data structures
    This commit updates the src/types.ts file to include new data structures for service, client, car,
    parts on service, and part. The updates include changing some type fields from number to string,
    adding new fields to the TService and TClient types, and renaming some fields. This will help
    improve the consistency and readability of the codebase, and make it easier to work with these data
    structures in other parts of the application.

--- src/types.ts
+++ src/types.ts
@@ -1,29 +1,41 @@
-export type TCar = {
-  id: number
-  brand: string
-  model: string
-}
-
-export type TClient = {
-  id: number
-  name: string
-  car: TCar
-}
-
 export type TContributor = {
-  id: number
-  name: string
-  service?: TService[]
+  id: string;
+  name: string;
+  service?: TService[];
 }
 
 export type TService = {
-  id: number
-  car: TCar
-  parts: TPart[]
+  id: string;
+  contributorId?: string;
+  clientId?: string;
+  initialService: string;
+  finishService: string | undefined;
+  client: TClient;
+  PartsOnService: TPartsOnService[];
+}
+
+export type TClient = {
+  id: string;
+  name: string;
+  carId: string;
+  car: TCar;
+}
+
+export type TCar = {
+  id: string;
+  brand: string;
+  model: string;
+}
+
+export type TPartsOnService = {
+  id: string;
+  serviceId: string;
+  partId: string;
+  part: TPart;
 }
 
 export type TPart = {
-  id: number
-  name: string
-  price: number
+  id: string;
+  name: string;
+  price: number;
 }

604882cc1c3f07d4364eff139a652f3b2ece6763
Kadu <kadu.silva2014@gmail.com>
2023-04-18 14:41:09 -0300

    ✨ feat: create inputName component

---
+++ src/components/InputNumberController/index.tsx
@@ -0,0 +1,29 @@
+import { InputNumber, InputNumberProps } from "primereact/inputnumber";
+import React from "react";
+import { Controller } from "react-hook-form";
+
+type InputNumberControllerProps = {
+  control: any,
+  nameControl: string,
+} & InputNumberProps
+
+const InputNumberController: React.FC<InputNumberControllerProps> = ({
+  control, nameControl, ...props
+}) => {
+  return (
+    <Controller
+      name={nameControl}
+      control={control}
+      render={({ field }) => (
+        <InputNumber
+          {...props}
+          id={field.name}
+          value={field.value}
+          onChange={(e) => field.onChange(e.value)}
+        />
+      )}
+    />
+  );
+};
+
+export default InputNumberController;

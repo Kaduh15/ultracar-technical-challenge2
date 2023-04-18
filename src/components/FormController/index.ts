56f3fd2ff5b6baa24881130ee3dccc5ee6acb9f9
Kadu <kadu.silva2014@gmail.com>
2023-04-18 17:36:09 -0300

    ♻️ refactor: refactoring of form components
    Refactoring of form components to improve code structure and standardization. Updated the
    src/components/FormController/index.ts file to export all form components including the
    InputTextController and ListBoxController. Updated
    src/components/FormController/InputTextController/index.tsx and
    src/components/FormController/ListBoxController/index.tsx files to use React Hook Form Controller
    and adjust props.

---
+++ src/components/FormController/index.ts
@@ -0,0 +1,5 @@
+export * from "./DropdownController";
+export * from "./MultiSelectController";
+export * from "./ListBoxController";
+export * from "./InputNumberController";
+export * from "./InputTextController";
